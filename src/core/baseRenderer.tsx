4fa205492d64b592f11c1f641790d5a28ef92fca
erictooth <1023110+erictooth@users.noreply.github.com>
2022-05-31 11:12:18 -0400

    chore: use `clsx` for merging classes in `baseRenderer`

--- src/core/baseRenderer.tsx
+++ src/core/baseRenderer.tsx
@@ -1,51 +1,40 @@
+import clsx from "clsx";
 import * as React from "react";
 import type { TableRenderer } from "./types/TableRenderer";
 
 export const baseRenderer: TableRenderer = {
   Table: (props) => (
-    <table
-      {...props}
-      className={`lean-table__table ${props.className || ""}`}
-    />
+    <table {...props} className={clsx("lean-table__table", props.className)} />
   ),
   Header: (props) => (
-    <thead
-      {...props}
-      className={`lean-table__header ${props.className || ""}`}
-    />
+    <thead {...props} className={clsx("lean-table__header", props.className)} />
   ),
   Body: (props) => (
-    <tbody {...props} className={`lean-table__body ${props.className || ""}`} />
+    <tbody {...props} className={clsx("lean-table__body", props.className)} />
   ),
   HeaderRow: (props) => (
     <tr
       {...props}
-      className={`lean-table__header-row ${props.className || ""}`}
+      className={clsx("lean-table__header-row", props.className)}
     />
   ),
   HeaderCell: (props) => (
     <th
       {...props}
-      className={`lean-table__header-cell ${props.className || ""}`}
+      className={clsx("lean-table__header-cell", props.className)}
     />
   ),
   Row: (props) => (
-    <tr
-      {...props}
-      className={`lean-table__body-row ${props.className || ""}`}
-    />
+    <tr {...props} className={clsx("lean-table__body-row", props.className)} />
   ),
   Cell: (props) => {
     const { className, columnId, ...rest } = props;
     return (
-      <td {...rest} className={`lean-table__body-cell ${className || ""}`} />
+      <td {...rest} className={clsx("lean-table__body-cell", className)} />
     );
   },
   Footer: (props) => (
-    <tfoot
-      {...props}
-      className={`lean-table__footer ${props.className || ""}`}
-    />
+    <tfoot {...props} className={clsx("lean-table__footer", props.className)} />
   ),
   reducer: (state) => state,
   modifyConfig: (configModifiers) => configModifiers,
