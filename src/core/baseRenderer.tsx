d584ef9d2a5d8fce8f3cc9c8eaa56fef16c3422b
erictooth <1023110+erictooth@users.noreply.github.com>
2022-05-31 19:20:42 -0400

    fix: wrap all `baseRenderer` elements with forwardRef

--- src/core/baseRenderer.tsx
+++ src/core/baseRenderer.tsx
@@ -3,34 +3,65 @@ import * as React from "react";
 import type { TableRenderer } from "./types/TableRenderer";
 
 export const baseRenderer: TableRenderer = {
-  Table: (props) => (
-    <table {...props} className={clsx("leantable__table", props.className)} />
-  ),
-  Header: (props) => (
-    <thead {...props} className={clsx("leantable__header", props.className)} />
-  ),
-  Body: (props) => (
-    <tbody {...props} className={clsx("leantable__body", props.className)} />
-  ),
-  HeaderRow: (props) => (
-    <tr {...props} className={clsx("leantable__header-row", props.className)} />
-  ),
-  HeaderCell: (props) => (
+  Table: React.forwardRef((props: any, ref) => (
+    <table
+      {...props}
+      className={clsx("leantable__table", props.className)}
+      ref={ref}
+    />
+  )) as any,
+  Header: React.forwardRef((props: any, ref) => (
+    <thead
+      {...props}
+      className={clsx("leantable__header", props.className)}
+      ref={ref}
+    />
+  )) as any,
+  Body: React.forwardRef((props: any, ref) => (
+    <tbody
+      {...props}
+      className={clsx("leantable__body", props.className)}
+      ref={ref}
+    />
+  )) as any,
+  HeaderRow: React.forwardRef((props: any, ref) => (
+    <tr
+      {...props}
+      className={clsx("leantable__header-row", props.className)}
+      ref={ref}
+    />
+  )) as any,
+  HeaderCell: React.forwardRef((props: any, ref) => (
     <th
       {...props}
       className={clsx("leantable__header-cell", props.className)}
+      ref={ref}
     />
-  ),
-  Row: (props) => (
-    <tr {...props} className={clsx("leantable__body-row", props.className)} />
-  ),
-  Cell: (props) => {
+  )) as any,
+  Row: React.forwardRef((props: any, ref) => (
+    <tr
+      {...props}
+      className={clsx("leantable__body-row", props.className)}
+      ref={ref}
+    />
+  )) as any,
+  Cell: React.forwardRef((props: any, ref) => {
     const { className, columnId, rowId, ...rest } = props;
-    return <td {...rest} className={clsx("leantable__body-cell", className)} />;
-  },
-  Footer: (props) => (
-    <tfoot {...props} className={clsx("leantable__footer", props.className)} />
-  ),
+    return (
+      <td
+        {...rest}
+        className={clsx("leantable__body-cell", className)}
+        ref={ref}
+      />
+    );
+  }) as any,
+  Footer: React.forwardRef((props: any, ref) => (
+    <tfoot
+      {...props}
+      className={clsx("leantable__footer", props.className)}
+      ref={ref}
+    />
+  )) as any,
   reducer: (state) => state,
   modifyConfig: (configModifiers) => configModifiers,
 };
