43f5f81598815f32d53dd90137fcc94b84e1ba3b
erictooth <1023110+erictooth@users.noreply.github.com>
2022-11-12 00:29:12 +0000

    fix: re-export all plugins from react module

--- src/react/plugins/index.ts
+++ src/react/plugins/index.ts
@@ -1,3 +1,9 @@
+export * from "../../plugins/column-pinning";
+export * from "../../plugins/column-resizing";
+export * from "../../plugins/column-visibility";
+export * from "../../plugins/infinite-scrolling";
+export * from "../../plugins/row-expansion";
+
 export * from "./column-sorting";
 export * from "./grid-layout";
 export * from "./row-selection";
