fa563cd34b4009af76daf60d3c10f3e8d42834a5
Christopher Yates <chris.yates@dremio.com>
2025-03-28 13:52:39 -0700

    Adding keyboard support for sortable headers cells

--- src/react/plugins/column-sorting/index.ts
+++ src/react/plugins/column-sorting/index.ts
@@ -37,6 +37,15 @@ export const columnSorting =
 								onClick: () =>
 									dispatch({ type: "SORT_COLUMN_TOGGLE", id: props.column.id }),
 								"aria-sort": sortDirection,
+								tabindex: 0,
+								onKeyDown: (e: KeyboardEvent) => {
+									if (e.key === "Enter") {
+									  dispatch({
+										type: "SORT_COLUMN_TOGGLE",
+										id: props.column.id,
+									  });
+									}
+								  },
 							};
 						},
 					};
