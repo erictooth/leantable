3b7ae1e278285a981b3a3995bbdcd0879c16e40d
erictooth <1023110+erictooth@users.noreply.github.com>
2025-05-12 17:30:11 -0700

    chore: export all types

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ export * from "./Table.tsx";
 export * from "./configContext.ts";
 export * from "./types/Column.ts";
 export * from "./types/InternalColumn.ts";
+export * from "./types/Options.ts";
+export * from "./types/Plugin.ts";
+export * from "./types/RenderedRow.ts";
 export type { Config };
